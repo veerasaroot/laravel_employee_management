684ee7d03d4b9298967637451c49146577cf2e35
Veerasaroot Bunkangwan <bosunakakawa@gmail.com>
2023-10-27 06:55:35 +0700

    Add Soft-UI Dashboard to the application.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('fontawesome/css/all.css') }}">
+
+    {{-- Soft UI --}}
+
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
+
+    <!-- Popper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.5/umd/popper.min.js"></script>
+    <!-- AlpineJS -->
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.10.3/cdn.min.js"></script>
+
+    <!-- Main Styling -->
+    <link href="{{ asset('css/styles.css?v=1.0.3') }}" rel="stylesheet" />
+
     @vite('resources/css/app.css')
     @stack('style')
 
@@ -26,6 +41,14 @@
 
 @yield('app')
 
+<!-- plugin for charts  -->
+<script src="{{ asset('js/plugins/chartjs.min.js') }}" async></script>
+<!-- plugin for scrollbar  -->
+<script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}" async></script>
+<!-- github button -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- main script file  -->
+<script src="{{ asset('js/soft-ui-dashboard-tailwind.js?v=1.0.3') }}" async></script>
 @stack('script')
 </body>
 </html>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -2,7 +2,13 @@
 
 @section('app')
     <x-sidebar/>
-    @yield('content')
+    <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+        @include('partials._dashboard.navbar')
+        <div class="w-full px-6 py-6 mx-auto">
+            @yield('content')
+        </div>
+    </main>
+
     <script src="{{ asset('flowbite/flowbite.min.js') }}"></script>
 @endsection
 
